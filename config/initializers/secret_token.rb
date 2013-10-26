83d0718ca3e4d8d0e180cd3557dbd5df202b14e3
kdurr <kristina.l.durr@gmail.com>
2013-10-25 21:24:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dct::Application.config.secret_key_base = '1600b356fe435e8f5169028e67331e3e6bbef4c2df5336163744b0e366e1a0b15dabc136299af77d05e7a36f2b0cdd9f67d597e7380fdf03c3ecaa8a637ad3a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dct::Application.config.session_store :cookie_store, key: '_dct_session'
